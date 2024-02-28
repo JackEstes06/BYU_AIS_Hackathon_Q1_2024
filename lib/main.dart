c0c9e57b0520f83210f0747245642c11bdded924
Jack Estes <jackestes3210@gmail.com>
2024-02-28 07:43:08 -0700

    Firebase setup since SQFlite likely wont work as DB option :(

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
+import 'package:ais_hackathon/db_sqlite.dart';
 import 'package:ais_hackathon/screens/users_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  FirebaseDatabase database = FirebaseDatabase.instance;
+
   runApp(const MyApp());
 }
 
@@ -15,7 +25,55 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      //home: const TestDBHelped(title: "Testing Database Helper"),
       home: const UsersScreen(),
+    );
+  }
+}
+
+class TestDBHelped extends StatefulWidget {
+  const TestDBHelped({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<TestDBHelped> createState() => _TestDBHelpedState();
+}
+
+class _TestDBHelpedState extends State<TestDBHelped> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.displayMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: const FloatingActionButton(
+        onPressed: testDatabaseHelper,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,18 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_analytics
+import firebase_auth
+import firebase_core
+import firebase_database
 import sqflite
 import sqlite3_flutter_libs
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
 }
